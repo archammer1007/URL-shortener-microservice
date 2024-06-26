65a3ccd16460f2de80c841ffb25d01c5eec8c251
Ben Martini <benmartini7C0@gmail.com>
2024-06-26 11:49:24 +0000

    feat:create website schema

--- index.js
+++ index.js
@@ -2,13 +2,27 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const app = express();
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+mongoose.connect(process.env.MONGO_URI, {userNewUrlParser: true, useUnifiedTopology: true});
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
+app.use(bodyParser.urlencoded({extended: false}));
+
 app.use('/public', express.static(`${process.cwd()}/public`));
+
+const Schema = mongoose.Schema;
+
+const websiteSchema = new Schema({
+  url: String
+});
+
+let Website = mongoose.model("Website", websiteSchema);
+
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
