67f8e637c34733d23c66c8cf9abbe68208f2a575
Ben Martini <benmartini7C0@gmail.com>
2024-06-29 01:20:32 +0000

    fix:add_https_to_longurl

--- index.js
+++ index.js
@@ -39,8 +39,10 @@ app.get('/api/hello', function(req, res) {
 
 app.post("/api/shorturl", function(req, res){
   //strip the http(s) from the website entered, because dns.lookup doesn't like it
-  var longurl = req.body.url.replace(/(^https?:\/\/)/, "");
-  dns.lookup(longurl, function(err, address, family){
+  var longurl = req.body.url;
+  //to use dns.lookup, we need to strip the http(s) from the website if it exists
+  var dnsaddress = req.body.url.replace(/(^https?:\/\/)/, "");
+  dns.lookup(dnsaddress, function(err, address, family){
     //if err is not null, then the website not not valid, so display an error message for the user
     if (err){
       res.json({error: "invalid url"})
